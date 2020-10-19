a38b318b7f46b2757a000ec0415313cd09a7b56f
Leonardo Schmitt <Leoeyeschmittk@gmail.com>
2020-10-18 22:08:39 -0300

    styling the button and random color

--- index.js
+++ index.js
@@ -1,39 +1,53 @@
 const icon = window.document.querySelector("i");
 const userValue = window.document.querySelector("input");
-const confirmBtn = window.document.querySelector('.confirmBtn');
-
-const options = [
-  "vuejs",
-  "react",
-  "angular",
-  "ember",
- 
-].sort();
+const confirmBtn = window.document.querySelector(".confirmBtn");
+const container = window.document.querySelector(".container");
+const options = ["vuejs", "react", "angular", "ember"].sort();
 
 function selectIcon() {
   let i = 0;
   for (i; i <= options.length - 1; i++) {
-    if (userValue.value.toLocaleUpperCase() === options[i].toLocaleUpperCase()) {
-     const chose = options[i].toLocaleUpperCase()
-      icon.className = `icon fab fa-${options[i].toLocaleLowerCase()}`;
-      confirmBtn.disabled = false
-          userValue.oninput = ()=> verify(chose)
+    if (
+      userValue.value.toLocaleUpperCase() === options[i].toLocaleUpperCase()
+    ) {
+      const chose = options[i];
+      icon.className = `icon fab fa-${options[i]}`;
+      confirmBtn.disabled = false;
+      randomColorIcon();
+
+      userValue.oninput = () => verify(chose);
     }
   }
 }
 
 function verify(chose) {
- let i = 0;
- for (i; i <= options.length - 1; i++) {
-   if (userValue.value != chose) {
-     icon.className = ``;
-    !confirmBtn.disabled
-     selectIcon()
-     
-         
-   }else {
-    icon.className = `icon fab fa-${chose.toLocaleLowerCase()}`;
-    confirmBtn.disabled = false
-   }
- }
+  let i = 0;
+  for (i; i <= options.length - 1; i++) {
+    if (userValue.value != chose) {
+      icon.className = ``;
+      confirmBtn.disabled = true;
+      selectIcon();
+    } else {
+      icon.className = `icon fab fa-${chose.toLocaleLowerCase()}`;
+      confirmBtn.disabled = false;
+      randomColorIcon();
+    }
+  }
+}
+
+function randomColorIcon() {
+  const colors = [
+    "red",
+    "yellow",
+    "blue",
+    "orange",
+    "purple",
+    "black",
+    "gray",
+    "green",
+    "pink",
+  ];
+
+  const res = Math.floor(Math.random() * colors.length);
+  icon.style.color = colors[res];
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
 # Datalist tag in HTML
+
+![screenshot](./screenshot.png)
+
+- [x] Use a datalist tag
+- [x] Style disabled and enable button
+- [x] Interact the result through random colors
+- [x] Learn something
